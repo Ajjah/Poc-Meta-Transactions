40d99303221bf197aafaea17a7fc1039b5db6d6e
Ajjah <hamzaajja123@gmail.com>
2022-05-24 14:11:50 +0200

    boutton switch

--- pages/index.js
+++ pages/index.js
@@ -232,8 +232,6 @@ const MetaTransaction = () => {
                 {!sent ? <button className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
                   onClick={sendToDefender}> Send Transaction </button > : <div className="bg-red-600 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
                   > Transaction Sent </div >}
-
-
               </div>
             </div>
           </div>
